3bec5a06b7f2b1c5a1092bef1c47a9ac11f9606a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-28 10:46:31 +0530

    adding libraries and defining nodes

--- quarterringstepview/src/main/java/com/anwesh/uiprojects/quarterringstepview/QuarterRingStepView.kt
+++ quarterringstepview/src/main/java/com/anwesh/uiprojects/quarterringstepview/QuarterRingStepView.kt
@@ -4,3 +4,13 @@ package com.anwesh.uiprojects.quarterringstepview
  * Created by anweshmishra on 28/10/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
+
