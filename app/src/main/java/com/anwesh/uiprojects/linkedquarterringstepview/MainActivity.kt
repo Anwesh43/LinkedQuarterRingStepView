6e2a4d98caed70071d4649d5561fd53eb03830a2
Anwesh43 <anweshthecool0@gmail.com>
2018-10-28 11:40:06 +0530

    adding QuarterRingStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedquarterringstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedquarterringstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedquarterringstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.quarterringstepview.QuarterRingStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        QuarterRingStepView.create(this)
     }
 }
